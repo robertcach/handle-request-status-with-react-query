789e3132f125a50ac1e808e8a25fa4ecf6f179cc
robertcach <cabrera.rch@gmail.com>
2024-04-18 19:55:24 +0200

    Create basic interfaces to type users fetch response

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import { getUsers } from './services/getUsers';
+import { User, UsersResponse } from './interfaces';
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
     const getUserRequest = async () => {
-      const data = await getUsers('1');
+      const data: UsersResponse = await getUsers('1');
 
       setUsers(data.data);
     };
@@ -29,7 +29,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      {users.map((user: any) => (
+      {users.map((user) => (
         <p key={user.id}>{user.first_name}</p>
       ))}
       <p className="read-the-docs">
