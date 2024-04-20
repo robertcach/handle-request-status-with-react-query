2e89cc37a46097892c45a91d70e1d9d6debd0e13
robertcach <cabrera.rch@gmail.com>
2024-04-20 11:54:06 +0200

    Change state to save all response request and show basic early returns

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,36 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import { getUsers } from './services/getUsers';
-import { User, UsersResponse } from './interfaces';
+import { UsersResponse } from './interfaces';
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState<UsersResponse | null>(null);
+  const [page, setPage] = useState(data?.page ?? 1);
 
   useEffect(() => {
+    setIsLoading(true);
     const getUserRequest = async () => {
-      const data: UsersResponse = await getUsers(page.toString());
-      setUsers(data.data);
+      try {
+        const data: UsersResponse = await getUsers(page.toString());
+        setData(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
     };
 
     getUserRequest();
   }, [page]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!data) {
+    <p>No data</p>;
+  }
 
   return (
     <>
@@ -29,12 +45,13 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      {users.map((user) => (
+      {data?.data?.map((user) => (
         <p key={user.id}>{user.first_name}</p>
       ))}
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
       <button onClick={() => setPage((prev) => prev + 1)}>Show more</button>
     </>
   );
